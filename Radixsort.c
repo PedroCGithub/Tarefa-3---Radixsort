66ebf574287c88482bff4743d3ce88abf4027223
PedroCGithub <pedro.casas.pequeno@gmail.com>
2025-05-23 21:40:07 +0000

    Finalizado

--- Radixsort.c
+++ Radixsort.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define ARRAY_SIZE 50
+#define MAX_VALUE 10000
 
 // Função para encontrar o maior elemento do array
-int getMax(int arr[], int n) {
+int achaMax(int arr[], int n) {
     int max = arr[0];
     for (int i = 1; i < n; i++) {
         if (arr[i] > max) {
@@ -12,9 +16,9 @@ int getMax(int arr[], int n) {
     return max;
 }
 
-// Função para realizar a ordenação por contagem (Counting Sort) para um dígito específico
+// Função para realizar a ordenação por contagem usando o Counting Sort npara um dígito específico
 void countingSort(int arr[], int n, int exp) {
-    int output[n]; // Array de saída
+    int saida[n]; // Array de saída
     int count[10] = {0}; // Inicializa o array de contagem
 
     // Armazena a contagem de ocorrências em count[]
@@ -29,20 +33,20 @@ void countingSort(int arr[], int n, int exp) {
 
     // Constrói o array de saída
     for (int i = n - 1; i >= 0; i--) {
-        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        saida[count[(arr[i] / exp) % 10] - 1] = arr[i];
         count[(arr[i] / exp) % 10]--;
     }
 
     // Copia o array de saída para arr[], agora contendo números ordenados pelo dígito atual
     for (int i = 0; i < n; i++) {
-        arr[i] = output[i];
+        arr[i] = saida[i];
     }
 }
 
-// Função principal que implementa o Radix Sort
+
 void radixsort(int arr[], int n) {
     // Encontra o número máximo para saber o número de dígitos
-    int max = getMax(arr, n);
+    int max = achaMax(arr, n);
 
     // Realiza a ordenação por contagem para cada dígito
     for (int exp = 1; max / exp > 0; exp *= 10) {
@@ -50,26 +54,25 @@ void radixsort(int arr[], int n) {
     }
 }
 
-// Função para imprimir um array
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
+int main() {
+    int arr[ARRAY_SIZE];
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int i;
+
+    srand(time(NULL));
+
+    printf("Array original randomizado:\n");
+    for (i = 0; i < ARRAY_SIZE; i++) {
+        arr[i] = rand() % MAX_VALUE;
         printf("%d ", arr[i]);
     }
-    printf("\n");
-}
-
-// Programa principal para testar as funções
-int main() {
-    int arr[] = {170, 45, 75, 90, 802, 24, 2, 66};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Array original:\n");
-    printArray(arr, n);
 
     radixsort(arr, n);
 
-    printf("Array ordenado:\n");
-    printArray(arr, n);
-
+    printf("\nArray ordenado:\n");
+        for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
     return 0;
 }
